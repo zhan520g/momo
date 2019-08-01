b871c614f1c60d2136174c79c49e34dee7ceaeb3
VSbug <15292299567@163.com>
2019-08-01 12:22:32 +0800

    增加NLog日志,并输入到mongoDb中

--- momo/Program.cs
+++ momo/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 
 namespace momo
 {
@@ -14,11 +16,32 @@ namespace momo
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //加载日志配置信息文件有后去捕获所的错误
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("Init Log API Information");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stop Log Information Because Of Exception");
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+             .UseStartup<Startup>()
+             .ConfigureLogging(logging =>
+             {
+                 logging.ClearProviders();//移除其它已经注册的日志处理程序
+                 logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);//记录最小日志级别
+             })
+            .UseNLog();//注入 NLog 服务
     }
 }
